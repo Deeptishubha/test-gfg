07f590e0fc5827fcc1957d0576860d7e656dec9b
Deepti Shubha <deeptishubha@gmail.com>
2025-07-15 20:19:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Maximum Perimeter of Triangle from array/maximum-perimeter-of-triangle-from-array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxPerimeter(int[] arr) {
+        
+        Arrays.sort(arr);
+        
+        for(int i=arr.length-1;i>=2;i--){
+            int a=arr[i];
+            int b=arr[i-1];
+            int c=arr[i-2];
+            
+            if(b+c >a){
+                return a+b+c;
+            }
+        }
+        return -1;
+    }
+}
