101f6548a6c0382d7e3b490739f30d5e84979abd
Deepti Shubha <deeptishubha@gmail.com>
2025-08-05 23:45:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Function With Return Value/function-with-return-value.java
@@ -0,0 +1,11 @@
+class Solution {
+    // code here
+    public static int returnValueFunction(int number) {
+       return 2*number;
+    }
+    public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+       returnValueFunction(a);
+}
+}
