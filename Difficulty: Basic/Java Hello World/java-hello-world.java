c0764a03a84c81d1839955487b7736b672109ef2
Deepti Shubha <deeptishubha@gmail.com>
2025-08-05 21:59:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Java Hello World/java-hello-world.java
@@ -0,0 +1,10 @@
+class Solution {
+    static void printHelloWorld() {
+        // code here
+        System.out.println("Hello World");
+    }
+    public static void main(String args[]){
+        Solution obj=new Solution();
+        obj.printHelloWorld();
+    }
+}
