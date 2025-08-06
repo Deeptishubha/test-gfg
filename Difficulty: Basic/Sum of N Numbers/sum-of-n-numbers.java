833bea9346ba4a2f64db30593fc778f8491cb451
Deepti Shubha <deeptishubha@gmail.com>
2025-08-06 23:41:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Sum of N Numbers/sum-of-n-numbers.java
+++ Difficulty: Basic/Sum of N Numbers/sum-of-n-numbers.java
@@ -1,17 +1,11 @@
+// User function Template for Java
 class Solution {
-    public static int nSum(int n) {
+    public int sumOfNumbers(int n) {
         // code here
-        int ans = 0;
+        int s=0;
         for(int i=1;i<=n;i++){
-            ans=ans+i;
+            s=s+i;
         }
-        return ans;
-    }
-    public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        Solution s=new Solution();
-        int r=s.nSum(n);
-        System.out.println(r);
+        return s;
     }
 }
