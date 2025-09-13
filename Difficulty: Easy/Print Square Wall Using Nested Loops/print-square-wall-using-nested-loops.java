f03cd4110bd7af9bc7008d652f842f4cfcf94dda
Deepti Shubha <deeptishubha@gmail.com>
2025-09-13 17:00:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Print Square Wall Using Nested Loops/print-square-wall-using-nested-loops.java
@@ -0,0 +1,11 @@
+class Solution {
+    public static void squareWall(int s) {
+        // Write your code here
+        for(int i=1;i<=s;i++){
+            for(int j=1;j<=s;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
