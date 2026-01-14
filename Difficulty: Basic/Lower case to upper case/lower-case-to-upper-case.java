ae83a176e6f3e300568062299f8999276e54655b
Deepti Shubha <deeptishubha@gmail.com>
2026-01-14 23:23:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Lower case to upper case/lower-case-to-upper-case.java
@@ -0,0 +1,8 @@
+// User function Template for Java
+class Solution {
+    String to_upper(String str) {
+        // code here
+       String upper=str.toUpperCase();
+        return upper;
+    }
+}
